519c228ee272117069a7a38c9a1eba32ca101576
thatanoslord <sabbarmoussa@gmail.com>
2024-11-16 02:24:50 +0100

    consol

--- console.py
+++ console.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
-"""Entry point of the command interpreter"""
+"""Defines the HBnB console."""
 import cmd
-import shlex  # Fix for shlex undefined
+import re
+from shlex import split
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+def parse(arg):
+    """Parse a command argument string into components."""
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
     """Holberton Bed & Breakfast (HBnB) Command Line class"""
 
     prompt = "(hbnb) "
-
-    # Valid classes for the create command
     valid_classes = {"BaseModel", "User", "State", "City", "Place", "Amenity", "Review"}
-
-    def do_EOF(self, line):
-        """Quit command to exit the program"""
-        print()
-        return True
-
-    def do_quit(self, line):
-        """Quit command to exit the program"""
-        return True
 
     def emptyline(self):
         """Called when an empty line is entered in response to the prompt"""
@@ -28,23 +44,71 @@ class HBNBCommand(cmd.Cmd):
             self.lastcmd = ""
             return self.onecmd('\n')
 
-    def do_create(self, arg):
-        """
-        Create a new instance of BaseModel and save it to the JSON file.
-        Usage: create <class_name>
-        """
-        commands = shlex.split(arg)
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
-        if len(commands) == 0:
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def do_create(self, arg):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        argl = parse(arg)
+        if len(argl) == 0:
             print("** class name missing **")
-        elif commands[0] not in self.valid_classes:
+        elif argl[0] not in HBNBCommand.valid_classes:
             print("** class doesn't exist **")
         else:
-            new_instance = eval(f"{commands[0]}()")
-            storage.new(new_instance)  # Ensure the object is added to storage
-            storage.save()  # Save changes to storage
-            print(new_instance.id)
+            print(eval(argl[0])().id)
+            storage.save()
 
 
-if __name__ == '__main__':
+
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Delete a class instance of a given id."""
+        argl = parse(arg)
+        objdict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.valid_classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
+            print("** no instance found **")
+        else:
+            del objdict["{}.{}".format(argl[0], argl[1])]
+            storage.save()
+
+    
+    
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
